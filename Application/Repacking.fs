3a83e2a34d592045b47874720457e9ae0aeec727
robot1 <robot1@space.org>
2022-04-08 00:15:04 +0200

    Example use-case in AL for viewing a bin tree

---
+++ Application/Repacking.fs
@@ -0,0 +1,14 @@
+﻿module StorageMachine.Repacking.Repacking
+
+open StorageMachine
+open Common
+open BinTree
+
+type IBinTreeDataAccess =
+
+    abstract RetrieveBinTree : BinIdentifier -> Option<BinTree>
+
+// A trivial use-case for retrieving a tree of bins based on the identifier of the outer bin. Result is 'None' if the
+// outer bin does not exist.
+let viewTree (dataAccess : IBinTreeDataAccess) (bin : BinIdentifier) : Option<BinTree> =
+    dataAccess.RetrieveBinTree bin
