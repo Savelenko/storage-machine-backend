2276933f8fa0f15f5fa78b80d8b817d39e37ba36
robot1 <robot1@space.org>
2022-04-07 22:50:50 +0200

    Include an example of POST request processing with JSON and error responses

--- Service/HttpHandlers.fs
+++ Service/HttpHandlers.fs
@@ -2,14 +2,12 @@
 
 open FSharp.Control.Tasks
 open Giraffe
-open Microsoft.AspNetCore.Authentication
-open Microsoft.AspNetCore.Authentication.Cookies
-open Microsoft.AspNetCore.Http
-open Microsoft.Extensions.Configuration
 open StorageMachine.Stock
 
 let requestHandlers : HttpHandler =
     choose [
         route "/hello" >=> GET >=> text "Storage machine is running"
+        // See the following example on how to process a POST request, decode JSON and return different responses
+        route "/number" >=> POST >=> PostExample.processPost
         Stock.stockHandlers
     ]
---
+++ Service/PostExample.fs
@@ -0,0 +1,20 @@
+﻿module StorageMachine.PostExample
+
+open FSharp.Control.Tasks
+open Microsoft.AspNetCore.Http
+open Giraffe
+open Thoth.Json.Giraffe
+open Thoth.Json.Net
+
+let processPost (next: HttpFunc) (ctx: HttpContext) =
+    task {
+        // Decode an integer number from JSON
+        let! inputNumber = ThothSerializer.ReadBody ctx Decode.int
+        match inputNumber with
+        | Error _ ->
+            return! RequestErrors.badRequest (text "POST body expected to consist of a single number") earlyReturn ctx
+        | Ok number when number % 2 = 0 ->
+            return! RequestErrors.notAcceptable (text "I don't want an even number") earlyReturn ctx
+        | Ok number ->
+            return! Successful.ok (text (sprintf "That's a nice odd number %d" number)) next ctx
+    }
